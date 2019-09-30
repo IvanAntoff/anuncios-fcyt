3d720cfebed2f7f03ff85253ba9a944d3a8d73a4
Ivan Antoff <1720106+IvanAntoff@users.noreply.github.com>
2019-09-30 12:09:15 -0300

    assents

---
+++ IniciarServiceWorker.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+      // Registro exitoso.
+      console.log('ServiceWorker registrado con exito: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker error durante el registro: ', err);
+    });
+  });
+}
