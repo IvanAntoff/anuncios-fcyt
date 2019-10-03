078bb47660f37aa9d95b11cd95fa35f506f81708
Ivan Antoff <1720106+IvanAntoff@users.noreply.github.com>
2019-10-03 13:54:53 -0300

    limpieza service worker

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,15 @@
 'use strict';
 
-var CACHE = 'Anuncios FCYT v0.1.0';
+var CACHE = 'Anuncios FCYT v0.2.0';
 var urlsToCache = [
 	'./',
-	'../views/home.hbs',
-	'/assets/index.html',
-	'/assets/js/helpers/helpers.js',
+	'./index.html',
 	'/assets/js/OneSignalSDKUpdaterWorker.js',
 	'/assets/js/OneSignalSDKWorker.js',
 	'/assets/js/date.js',
 	'/assets/css/style.css',
 	'/assets/database/database.js',
-	'/assets/database/data.json', 				//nota para el desarrollador, upgradea esto kbza
-	'/assets/images/icons/FB2.png', 				//nota para el desarrollador, upgradea esto kbza
-	'/assets/images/icons/GH2.png', 				//nota para el desarrollador, upgradea esto kbza
-	'/assets/images/icons/TW2.png', 				//nota para el desarrollador, upgradea esto kbza
-	'/assets/images/icons/IG2.png',	 			//nota para el desarrollador, upgradea esto kbza
+	'/assets/database/data.json', 					//nota para el desarrollador, upgradea esto kbza
 	'/assets/images/assents/header.png' 			//nota para el desarrollador, upgradea esto kbza
 ];
 
